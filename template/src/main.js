24c11ce0469f0ebbd118a8fb1ecb4ffa06450d76
Ted Patrick <ted@light.ly>
2017-01-24 08:27:19 -0600

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -2,10 +2,7 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-
-window.app = {};
-app.model = {};
-                         
+       
 new Vue({
   el: '#app',
   {{#if_eq build "runtime"}}
@@ -13,9 +10,6 @@ new Vue({
   {{/if_eq}}
   {{#if_eq build "standalone"}}
   template: '<App/>',
-  data(){ 
-    return { model:app.model } 
-  },
   components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
