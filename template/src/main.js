645e9f991c0ceebe02a935e42235d1641beca850
Ted Patrick <ted@vestu.com>
2017-01-25 18:40:10 -0600

    update 1

--- template/src/main.js
+++ template/src/main.js
@@ -2,7 +2,31 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-       
+import request from 'superagent';
+
+//shared methods
+import methods from './modules/methods'
+
+//shared model data
+import model from './modules/model'
+
+var app = {}
+app.model = model;
+app.services = services;
+app.methods = methods;
+window.app = app;
+
+request.get( '/static/meetup.json' ).end(function(err, res){
+    if( err ){
+      console.log( err );
+      return;
+    }else{
+      var result = JSON.parse( res.text );
+      console.log( result );
+      model.members = result.results;
+    }
+});
+
 new Vue({
   el: '#app',
   {{#if_eq build "runtime"}}
--- template/src/modules/services.js
+++
@@ -1,5 +0,0 @@
-var moduleData = {};
-
-
-
-module.exports = moduleData;
